3921c6e2cd8edf5c997fdc8b33c7d77f75d22c7c
Kaspar Schleiser <kaspar@schleiser.de>
2017-09-14 10:44:32 +0200

    boards: samr21-xpro: disable PA19 for pwm (collides with led)

--- boards/samr21-xpro/include/periph_conf.h
+++ boards/samr21-xpro/include/periph_conf.h
@@ -166,7 +166,8 @@ static const pwm_conf_t pwm_config[] = {
         /* GPIO pin, MUX value, TCC channel */
         { GPIO_PIN(PA, 16), GPIO_MUX_F, 0 },
         { GPIO_PIN(PA, 18), GPIO_MUX_F, 2 },
-        { GPIO_PIN(PA, 19), GPIO_MUX_F, 3 }
+        { GPIO_UNDEF, (gpio_mux_t)0, 2 }
+        //{ GPIO_PIN(PA, 19), GPIO_MUX_F, 3 }
     }}
 #endif
 };

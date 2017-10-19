1cd5a3dacb58863ff560040d63ecf60210603e0b
Kaspar Schleiser <kaspar@schleiser.de>
2017-10-19 18:03:07 +0200

    examples/javascript/lwm2m.c gcoap clist fixup

--- examples/javascript/lwm2m.c
+++ examples/javascript/lwm2m.c
@@ -128,8 +128,7 @@ static const coap_resource_t _resources[] = {
 };
 static gcoap_listener_t _listener = {
     (coap_resource_t *)&_resources[0],
-    sizeof(_resources) / sizeof(_resources[0]),
-    NULL
+    sizeof(_resources) / sizeof(_resources[0])
 };
 
 /*

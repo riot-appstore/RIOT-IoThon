927c4d97020942071c2e3933df280560c704a2d0
danpetry <daniel.petry@fu-berlin.de>
2018-01-18 20:06:20 +0100

    Added empty threads for the three processes
    - Producer (will collect data from sensors)
    - Processor (will process data)
    - Sender (will send data to server)

--- examples/iothon_app/main.c
+++ examples/iothon_app/main.c
@@ -24,12 +24,21 @@
 #include "net/gcoap.h"
 #include "kernel_types.h"
 #include "shell.h"
+#include "thread.h"
 
 #define MAIN_QUEUE_SIZE (4)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
 extern int gcoap_cli_cmd(int argc, char **argv);
 extern void gcoap_cli_init(void);
+
+extern void *data_processor(void *arg);
+extern void *data_producer(void *arg);
+extern void *data_sender(void *arg);
+
+char data_processor_stack[THREAD_STACKSIZE_MAIN];
+char data_producer_stack[THREAD_STACKSIZE_MAIN];
+char data_sender_stack[THREAD_STACKSIZE_MAIN];
 
 static const shell_command_t shell_commands[] = {
     { "coap", "CoAP example", gcoap_cli_cmd },
@@ -41,8 +50,26 @@ int main(void)
     /* for the thread running the shell */
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
     gcoap_cli_init();
-    puts("gcoap example app");
+    puts("iothon example app");
 
+    kernel_pid_t proc_pid = thread_create(data_processor_stack, sizeof(data_processor_stack),
+                            THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST,
+                            data_processor, NULL, "pong");
+    
+    kernel_pid_t prod_pid = thread_create(data_producer_stack, sizeof(data_producer_stack),
+                            THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST,
+                            data_producer, NULL, "pong");
+
+    kernel_pid_t send_pid = thread_create(data_sender_stack, sizeof(data_sender_stack),
+                            THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST,
+                            data_sender, NULL, "pong");
+    
+   
+    (void)proc_pid;
+    (void)prod_pid;
+    (void)send_pid;
+    
+    
     /* start shell */
     puts("All up, running the shell now");
     char line_buf[SHELL_DEFAULT_BUFSIZE];

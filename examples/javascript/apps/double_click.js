091c76948b1bdf90e9b2eefae8f8551cd8761d6c
danpetry <daniel.petry@fu-berlin.de>
2018-03-13 10:46:59 +0100

    Examples/javascript: some mods, can't remember
    Some changes. I think these were what Kaspar did to help it work
    with a 6LoWPAN network but can't really remember. At this point
    I was talking about setting up a network with Martine.

--- examples/javascript/apps/double_click.js
+++ examples/javascript/apps/double_click.js
@@ -55,7 +55,10 @@ var publisher_app = new function() {
         print("publish: event CB: " + value);
         // PUT 3
         payload = value.toString();
-        coap.request("coap://[fd00:dead:beef::1]/click", 3, payload);
+        coap.request("coap://[2001:470:749d:2:7b76:4044:da06:8aba]/click", coap.method.PUT, payload);
+//        coap.request("coap://[fe80::7b76:4044:da06:8aba]/riot/board", 3, payload);
+//        retval = coap.request("coap://[fe80::7b76:4044:da06:8aba]:5683/riot/board", coap.method.GET, "276896");
+//	print("data received: " + retval);
         return true;
     }
 }
